c419712910b22ecf8e807092e21842a71b058395
Shaun Hall <shaun.hall@thehutgroup.com>
2024-01-22 10:36:32 +0000

    set up pom and ai class

--- src/main/java/com/thg/accelerator23/connectn/ai/shaun/SirConnectABitMore.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/shaun/SirConnectABitMore.java
@@ -1,14 +1,14 @@
-package com.thg.accelerator23.connectn.ai.yourteam;
+package com.thg.accelerator23.connectn.ai.shaun;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
 
-public class NameAi extends Player {
-  public NameAi(Counter counter) {
+public class SirConnectABitMore extends Player {
+  public SirConnectABitMore(Counter counter) {
     //TODO: fill in your name here
-    super(counter, NameAi.class.getName());
+    super(counter, SirConnectABitMore.class.getName());
   }
 
   @Override
